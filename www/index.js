d7c34b3a0ebabffd7d3f0c9e4e50b68ef230de43
silverbirder <silverbirder@gmail.com>
2020-12-07 11:25:54 +0000

    🛠 Delete unused code

--- www/index.js
+++ www/index.js
@@ -1,6 +1,7 @@
-import { greet, FizzBuzz } from "wasm-game-of-life";
+import { FizzBuzz } from "wasm-game-of-life";
 
 const fb = FizzBuzz.new();
 fb.call();
 fb.increment();
 fb.call();
+console.log(fb);
